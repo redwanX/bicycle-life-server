2c41184fcc60afff4ffd546353a274a7e17400cf
redwanX <redwanistbd2@gmail.com>
2022-05-24 01:53:19 +0600

    token Generate

--- index.js
+++ index.js
@@ -16,22 +16,23 @@ app.get('/',(req,res)=>{
 })
 
 //MONGODB CONNECTION
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uttsl.mongodb.net/?retryWrites=true&w=majority";`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uttsl.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const run = async()=>{
     try{
         await client.connect();
-        const products = client.db('furnitures').collection('parts');
+        const products = client.db('parts').collection('parts');
+        const users = client.db('parts').collection('users');
         
-        //GET ALL TOOLS
-        app.get('/alltools',async(req,res)=>{
+        //GET ALL PARTS
+        app.get('/allparts',async(req,res)=>{
             query={};
             const cursor = products.find(query);
             const result= await cursor.toArray();
             res.send(result);
             });
-        //GET A SINGLE TOOL BY ID
-        app.get('/tool/:id',async(req,res)=>{
+        //GET A SINGLE PART BY ID
+        app.get('/part/:id',async(req,res)=>{
                 try{
                 const id = req?.params?.id
                 const query = {_id:ObjectId(id)}
@@ -42,6 +43,20 @@ const run = async()=>{
                     res.send({})
                 }
             });
+        //CREATING JWT TOKEN
+        app.put('/login/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+              $set: user,
+            };
+            const result = await users.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.JWT_SECRET, { expiresIn: '1d' })
+            console.log(token);
+            res.send({ result, token });
+          });
     }
     finally{
     }
