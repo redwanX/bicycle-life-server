977e34532af53dff055a80f614fab0348b3e02f8
redwanX <redwanistbd2@gmail.com>
2022-05-23 16:57:43 +0600

    connected to mongo

--- index.js
+++ index.js
@@ -10,12 +10,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
 // Index
 app.get('/',(req,res)=>{
     res.send("server is running");
 })
+
+//MONGODB CONNECTION
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uttsl.mongodb.net/?retryWrites=true&w=majority";`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const run = async()=>{
+    try{
+        await client.connect();
+        console.log("db connected")
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+
+
 
 //LISTENING TO PORT
 app.listen(port,()=>{console.log(`listening to port: ${port}`)});
